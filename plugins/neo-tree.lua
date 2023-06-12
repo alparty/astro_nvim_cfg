91bf00275f95bb2216614c07dc414d5ea3fc762b
alex <23722276+alparty@users.noreply.github.com>
2023-06-12 16:10:48 +0200

    neo-tree do not follow cursor

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
               open_on_setup = true,
               open_on_setup_file = false
             },
-            follow_current_file = true,
+            follow_current_file = false,
             hijack_netrw_behavior = "open_current",
             use_libuv_file_watcher = true,
           },
